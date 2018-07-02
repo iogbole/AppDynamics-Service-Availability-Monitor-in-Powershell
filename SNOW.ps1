82130ebb397b56076ad82c9b5d9c9c797c5b5587
Israel Ogbole <iogbole@yahoo.com>
2018-07-02 14:14:44 +0100

    initial commit

---
+++ SNOW.ps1
@@ -0,0 +1,37 @@
+Clear-Host
+
+$tier_id = "XXX"
+$service_name = "SNOW"
+$metric_name = "Availability"
+$metric_prefix = "name=Server|Component:$tier_id|Custom Metrics|Service Availability|$service_name|$metric_name,"
+
+$success = "value=1"
+$failed = "value=0"
+
+#https://FQDN:8080/
+
+$url = 'https://appdynamics.com' #replace with the url you'd like to monitor. 
+$req = [system.Net.WebRequest]::Create($url)
+
+$req.Timeout = 30000 # 30 seconds 
+$req.AllowAutoRedirect = $true # set this to $false to disable follow redirects 
+
+try {
+    $res = $req.GetResponse()
+} 
+catch [System.Net.WebException] {
+    $res = $_.Exception.Response
+}
+$res.StatusCode
+#OK
+[int]$res.StatusCode
+#200 
+if ([int]$res.StatusCode -eq 200 ) { 
+    Write-Host "Service respondsed with 200 OK. Send 1 to AppDynamics"
+    Write-Host $metric_prefix$success
+}
+else {
+    Write-Host "Service failed to respond. Send zero to AppDynamics"
+    Write-Host $metric_prefix+$failed
+
+}
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+Powershell.exe -ExecutionPolicy remotesigned -File GeoServer.ps1
+
+Powershell.exe -ExecutionPolicy remotesigned -File SNOW.ps1
